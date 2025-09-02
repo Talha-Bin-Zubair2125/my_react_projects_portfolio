0d51f3b098e597cd4f3475ff2ccd5bdd25b54e3d
Talha-Bin-Zubair <talhazubair2125@gmail.com>
2025-09-03 01:33:43 +0500

    Updated

--- login_signup_react/src/App.jsx
+++ login_signup_react/src/App.jsx
@@ -4,11 +4,11 @@ import Signup from "./Pages_for_login_signup/Signup"
 
 function App() {
   
-
   return (
     <>
     <BrowserRouter>   
       <Routes>
+        <Route path="/" element={<Signup/>}>Login</Route>    
        <Route path="/Login" element={<Login/>}>Login</Route>    
        <Route path="/Signup" element={<Signup/>}>Signup</Route>    
       </Routes>       
